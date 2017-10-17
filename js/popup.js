e228288157034d9901ca5a11aa223b27032f46b2
Carey Kwan <845870774@ken20-202-16.local>
2017-10-17 13:45:09 +1300

    cafe app

---
+++ js/popup.js
@@ -0,0 +1,42 @@
+// This is order button for ipad
+$(document).ready(function(){
+
+	$("#popUpButton").click(function(){
+		$("#popUp").show();
+	})
+	$("#closeTab").click(function(){
+		$("#popUp").hide();
+	})
+});
+
+// this is the pop up for bell
+$(document).ready(function(){
+
+	$("#bellButton").click(function(){
+		$("#popUp1").show();
+	})
+	$("#close").click(function(){
+		$("#popUp1").hide();
+	})
+});
+// this is the js for iphone order button
+$(document).ready(function(){
+
+	$("#popUpButtonPhone").click(function(){
+		$("#popUp2").show();
+	})
+	$("#close2").click(function(){
+		$("#popUp2").hide();
+	})
+});
+
+// this is the js for bill order button
+$(document).ready(function(){
+
+	$("#popUpButton4").click(function(){
+		$("#popUp3").show();
+	})
+	$("#close3").click(function(){
+		$("#popUp3").hide();
+	})
+});
